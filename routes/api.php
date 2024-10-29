125cebbc9e072b286378533587d10f7714e3eb83
Khaled Moussa <khaledmoussa909@gmail.com>
2024-10-29 15:55:16 +0300

    Upload 1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\WebhookController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/webhook/paymob', [WebhookController::class, 'handleWebhook']);
-Route::post('/webhook/paymob/response', [WebhookController::class, 'handleTransactionResponse']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/webhook/paymob', [WebhookController::class, 'handleWebhook']);
+Route::post('/webhook/paymob/response', [WebhookController::class, 'handleTransactionResponse']);
